6b68a3dadb024e54bca025fcf863e6dcd7569867
Mohit9113 <mohitkjanardhan@gmail.com>
2025-10-03 14:33:41 +0530

    Add files via upload

---
+++ sales_trend_analysis.sql
@@ -0,0 +1,13 @@
+-- Task 6: Sales Trend Analysis Using Aggregations
+
+SELECT
+    EXTRACT(YEAR FROM order_date) AS order_year,
+    EXTRACT(MONTH FROM order_date) AS order_month,
+    SUM(amount) AS total_revenue,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM
+    online_sales
+GROUP BY
+    order_year, order_month
+ORDER BY
+    order_year, order_month;
